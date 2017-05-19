81640fa3ce119860ac1a10df5f4c46a8767ca0ff
aburke <aaron.burke@hallmarkbusinessconnections.com>
2017-05-19 12:58:06 -0500

    Update with dump.js files

---
+++ Auth0-dump-test-API.js
@@ -0,0 +1,161 @@
+#! /usr/bin/env node
+
+const request = require('request');
+const fs = require('fs');
+const path = require('path');
+const mkdirp = require('mkdirp').sync;
+const program = require('./lib/cli');
+const winston = require('./lib/logger');
+const authenticate = require('./lib/auth');
+//----------------------------------------------------------------------------------------------------------------------
+
+program
+.option('-o,--output_dir <output dir>', 'The directory where the config will be written. See README and online for more info.')
+.parse(process.argv);
+
+if (!program.output_dir) {
+	program.printHelpAndExit('Must set the output directory');
+}
+
+const config = require( path.isAbsolute(program.config_file) ? program.config_file : path.join(process.cwd(), program.config_file) );
+const DIR = path.isAbsolute(program.output_dir) ? program.output_dir : path.join(process.cwd(), program.output_dir);
+var TOKEN = null;
+
+mkdirp(DIR);
+//----------------------------------------------------------------------------------------------------------------------
+
+/* function getjson(entityName) {
+	return new Promise((resolve, reject) => {
+		request({
+			url: 'https://'+config.AUTH0_DOMAIN+'/api/v2/'+entityName,
+			headers: {
+				'Authorization': 'Bearer '+TOKEN,
+				'Cache-Control': 'no-cache',
+			}
+		},
+		function (error, response, body) {
+			if (error) {
+				reject(error);
+			}
+			else if (response.statusCode != 200) {
+				reject(new Error("HTTP code : " + response.statusCode + "; " + body));
+			}
+			else {
+				winston.info("Got "+entityName);
+				var json = JSON.parse(body);
+				resolve(json);
+			}
+		});
+	})
+} */
+//----------------------------------------------------------------------------------------------------------------------
+
+function writeRules() {
+	return function(json) {
+		var jsonToWrite = [];
+		mkdirp(path.join(DIR,'rules'));
+		for (let item of json) {
+			item = Object.keys(item).sort().reduce((r, k) => {r[k] = item[k]; return r}, {});
+			var name = item.name;
+			var filePath = path.join(DIR, 'rules', name+".js");
+			fs.writeFileSync(filePath, item.script);
+			winston.info(" - Wrote", 'rules', name+".js");
+			
+			var itemToWrite = Object.assign({}, item);
+			delete itemToWrite.script;
+			jsonToWrite.push(itemToWrite);
+		}
+		
+		return writeFiles('rules')(jsonToWrite);
+	}
+}
+
+
+function writeFiles(dir, extension = '.json') {
+	mkdirp(path.join(DIR,dir));
+	return function(json) {
+		for (let item of json) {
+			item = Object.keys(item).sort().reduce((r, k) => {r[k] = item[k]; return r}, {});
+			var name = item.name;
+			var filePath = path.join(DIR, dir, name+extension);
+			fs.writeFileSync(filePath, JSON.stringify(item,null,'\t'));
+			winston.info(" - Wrote", dir, name+extension);
+		}
+	}
+}
+
+function writeConnections() {
+	return function(json) {
+		mkdirp(path.join(DIR,'database-connections'));
+		for (let item of json) {
+			if (item.options.customScripts && Object.keys(item.options.customScripts).length > 0) {
+				var name = item.name;
+				mkdirp(path.join(DIR,'database-connections',name));
+				
+				[
+					'get_user',
+					'create',
+					'verify',
+					'login',
+					'change_password',
+					'delete',
+				].forEach((file) => {
+					if (item.options.customScripts[file]) {
+						var fpath = path.join(DIR,'database-connections',name, file+".js");
+						fs.writeFileSync(fpath, item.options.customScripts[file]);
+						winston.info(" - Wrote", fpath);
+					}
+				});
+				delete item.options.customScripts;
+			}
+		}
+		//writeFiles('connections')(json);
+	}
+}
+//----------------------------------------------------------------------------------------------------------------------
+
+function writePages(dir, extension = '.html') {
+	mkdirp(path.join(DIR,dir));
+	return function(json) {
+		for (let item of json) {
+			item = Object.keys(item).sort().reduce((r, k) => {r[k] = item[k]; return r}, {});
+			var name = item.name;
+			var hostedpage = item.custom_login_page_preview;
+			var filePath = path.join(DIR, dir, name+extension);
+			if (item.custom_login_page_on) {
+				fs.writeFileSync(filePath, hostedpage);
+				winston.info(" - Wrote", dir, name+extension);
+			}
+		}
+	}
+}
+
+var ManagementClient = require('auth0').ManagementClient;
+winston.info("Dumping configuration to "+DIR);
+authenticate(config)
+.then((token) => {TOKEN = token})
+.then(() => {
+	var management = new ManagementClient({
+		token: TOKEN,
+		domain: config.AUTH0_DOMAIN
+	});
+	
+	var getConnections = management.getConnections()
+		.then(writeConnections());
+	var getClients = management.getClients()
+		.then((clients) => clients.filter((c) => c.global != true))
+		.then((clients) => clients.filter((c) => c.client_id != config.AUTH0_CLIENT_ID))
+		.then(writeFiles('clients'));
+	var getRules = management.getRules()
+		.then(writeRules());
+	var getPages = management.getClients()
+		.then((clients) => clients.filter((c) => c.global != true))
+		.then((clients) => clients.filter((c) => c.custom_login_page_on != false))
+		.then(writePages('pages'));
+	
+	return Promise.all([getConnections, getClients, getRules, getPages])
+})
+.catch(e => {
+	winston.error(e);
+	process.exit(1);
+});
